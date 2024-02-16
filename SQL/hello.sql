9d49a82a4c5117ab58fbeaff1cd62350ec2e4cfa
Vivek Kumar <vk270599@gmail.com>
2024-02-16 23:33:56 +0530

    2024-02-17

--- SQL/hello.sql
+++ SQL/hello.sql
@@ -1,5 +1,14 @@
 - https://leetcode.com/problems/invalid-tweets/?envType=study-plan-v2&envId=top-sql-50
 select tweet_id from tweets where length(content) > 15
 
-
-
+- https://leetcode.com/problems/department-top-three-salaries/description/?envType=study-plan-v2&envId=top-sql-50
+select a.Department, a.Employee, a.Salary
+from
+(
+    select d.name as Department, e.name as Employee, e.salary as Salary, 
+    dense_rank() over (partition by d.id order by e.salary desc) as ranking
+    from employee as e
+    inner join department as d
+    on e.departmentId = d.id
+) a
+where a.ranking <= 3
