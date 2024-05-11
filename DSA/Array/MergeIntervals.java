4d4ebc119b1955bd61122b408f57e93599adf6e2
Vivek Kumar <vk270599@gmail.com>
2024-05-11 23:51:59 +0530

    sync-for-date-2024-05-11

--- DSA/Array/MergeIntervals.java
+++ DSA/Array/MergeIntervals.java
@@ -3,44 +3,10 @@ import java.util.*;
 
 
 public class MergeIntervals{
+    // Merge all overlapping intervals,
   
   // Approch 1 - Time: O(n log(n)) + O(n)  and Space: O(n) {for storing the result}
-  public int[][] merge2(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        int i = 0;
-        while(i < intervals.length){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            for(int j=i+1; j<intervals.length; j++){
-                if(start <= intervals[j][0] && intervals[j][0] <= end){
-                    start = Math.min(start, intervals[j][0]);
-                    end = Math.max(end, intervals[j][1]);
-                    i = j;
-                }
-                else {
-                    break;
-                }
-            }
-            i++;
-            ArrayList<Integer> elem = new ArrayList<Integer>();
-            elem.add(start);
-            elem.add(end);
-            result.add(elem);
-        }
-        int[][] ans = new int[result.size()][2];
-        i=0;
-        for(ArrayList<Integer> elem: result){
-            ans[i][0] = result.get(i).get(0);
-            ans[i][1] = result.get(i).get(1);
-            i++;
-        }
-        return ans;
-    }
-
-    // Approch 1 [could be coded better using LinkedList]
-
-    public int[][] merge3(int[][] intervals) {
+    public int[][] merge2(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
         LinkedList<int[]> result = new LinkedList<int[]>();
         int i = 0;
@@ -63,8 +29,8 @@ public class MergeIntervals{
         return result.toArray(new int[result.size()][2]);
     }
 
-    // numbers are only positive => Traverse and use a Hash to store the values seen
-    // Time: O(N) 
+    // Numbers are only positive => Traverse and use a Hash to sort the values
+    // Time: 2 * O(N) + 10^4 
     public int[][] merge(int[][] intervals) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -99,5 +65,4 @@ public class MergeIntervals{
 
     }
 
-  
 }
