96b2409ab25a8f53cc320d02277ca3c4148d5605
Vivek Kumar <vk270599@gmail.com>
2024-05-12 11:01:25 +0530

    sync-for-date-2024-05-12

--- DSA/Run.java
+++ DSA/Run.java
@@ -11,7 +11,7 @@ class Run {
   
     SortZeroOneTwo t = new SortZeroOneTwo();
     int[] arr = {2,2,0,0,0,1,1,1};   
-    int[] ans = t.sortColors(arr);
+    //int[] ans = t.sortColors(arr);
     //for(int i : ans) System.out.println(i);
 
     FindtheUnion f = new FindtheUnion();
