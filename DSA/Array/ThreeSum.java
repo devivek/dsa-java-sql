48581d22f23b3bd47861e51f63c85af061b52e89
Vivek Kumar <vk270599@gmail.com>
2024-05-19 16:23:52 +0530

    sync-for-date-2024-05-19

--- DSA/Array/ThreeSum.java
+++ DSA/Array/ThreeSum.java
@@ -4,12 +4,27 @@ import java.util.*;
 // [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0
 // The solution set must not contain duplicate triplets.
 public class ThreeSum {
+
+    // Approch-1: Brute Force: O(N^3) * (sort the triplet) * log(no. of unique triplets) Check for each possible pair
+    
     public List<List<Integer>> threeSum(int[] nums) {
-        // Approch-1: Brute Force: O(N^3) * (sort the triplet) * log(no. of unique triplets) Check for each possible pair
+        
 
         // Approch-2: Same as twoSum with a target -> Could be done in O(N^2) 
+        (1, 2 , 3) (2, -2, 6)
         
+
         //-> If we use hash, we can do in O(N)
 
+
+
+
+        
+
+
     }
+
+
+    // sort the array -> then use two pointer approch of two sum
+
 }
