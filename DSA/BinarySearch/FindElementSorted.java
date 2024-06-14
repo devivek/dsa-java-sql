7bc29fef1bcb717195df1df2560a029d115af2c3
vivek <vivekkumar@mobilewalla.com>
2024-06-14 21:02:13 +0530

    sync-date-2024-06-14

--- DSA/BinarySearch/FindElementSorted.java
+++ DSA/BinarySearch/FindElementSorted.java
@@ -1,12 +1,13 @@
 package BinarySearch;
 
-public class FindElement {
+import java.util.Arrays;
 
-    // Binary Search
+public class FindElementSorted {
+
+    // Binary Search : Time: O(logN) and Space: O(1)
         // 1. Define the range for Search;
         // 2. Condition to divide the array in terms of True or False;
         // 3. Find the Boundary, either maximizing or minimizing the index
-    // Time: O(logN) and Space: O(1)
     static public int search(int[] nums, int target) {
         int low = 0;
         int high = nums.length-1;
@@ -75,17 +76,57 @@ public class FindElement {
         return result;
     }
 
+    // Find the starting and ending position of a given target value.
+    // FIRST: [2, 4, 4, 6, 7, 9] => target = 4 => (arr[i] >= target) => F, T, T, T, T, T => 1 : Minimum index where it is true
+    // LAST: [2, 4, 4, 6, 7, 9] => target = 4 => (arr[i] <= target) => T, T, T, F, F, F => 2 : Maximum index where it is true
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[]{-1, -1};
+        int low = 0;
+        int high = nums.length-1;
+
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                result[0] = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        low = 0;
+        high = nums.length-1;
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                result[1] = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] <= target){
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
         long[] arr_long = {1, 22, 53, 466, 58888, 665666};
         int target = 44;
         System.out.println("Binary Search");
-        System.out.println(FindElement.search(arr, target));
+        System.out.println(FindElementSorted.search(arr, target));
         System.out.println("Floor");
-        int floor_index = FindElement.findFloor(arr_long, 6, 50);
+        int floor_index = FindElementSorted.findFloor(arr_long, 6, 50);
         System.out.println(floor_index == -1 ? -1 : arr_long[floor_index]);
         System.out.println("Ceil");
-        int ceil_index = FindElement.findCeil(arr_long, 6, 50);
+        int ceil_index = FindElementSorted.findCeil(arr_long, 6, 50);
         System.out.println(ceil_index == -1 ? -1 : arr_long[ceil_index]);
+
+        int[] arr_long_2 = {1, 22, 22, 22, 45, 66};
+        System.out.println("searchRange");
+        System.out.println(Arrays.toString(FindElementSorted.searchRange(arr_long_2, 22)));
     }
 }
