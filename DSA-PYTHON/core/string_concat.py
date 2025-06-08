b3e66f0dda516301d0793893dbf30152a0e861f3
vivek <vk270599@gmail.com>
2025-06-08 20:10:00 +0530

    sync-date-2025-06-08

---
+++ DSA-PYTHON/core/string_concat.py
@@ -0,0 +1,21 @@
+import timeit
+
+# Concatenate strings
+start_time = timeit.default_timer()
+s = ''
+for i in range(100000000):
+    s += 'abcd'
+elapsed_time_concat = timeit.default_timer() - start_time
+print('Time taken for string concatenation:', elapsed_time_concat)
+
+# Join strings
+start_time = timeit.default_timer()
+lst = []
+for i in range(100000000):
+    lst.append('abcd')
+s = ''.join(lst)
+elapsed_time_join = timeit.default_timer() - start_time
+print('Time taken for string join:         ', elapsed_time_join)
+
+join_performance = (elapsed_time_concat - elapsed_time_join) / elapsed_time_concat
+print('Performance gained with join', join_performance)
