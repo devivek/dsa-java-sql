cd4993fbe620e92c996500acd23156008bbd1cf5
vivek <vk270599@gmail.com>
2025-06-11 20:10:00 +0530

    sync-date-2025-06-11

--- DSA-PYTHON/core/string_concat.py
+++ DSA-PYTHON/core/string_concat.py
@@ -1,4 +1,10 @@
 import timeit
+import os, sys, platform, multiprocessing
+
+print("Python:", sys.version)
+print("Interpreter:", sys.executable)
+print("Platform:", platform.platform())
+print("Logical CPUs :", multiprocessing.cpu_count())
 
 # Concatenate strings
 start_time = timeit.default_timer()
@@ -15,7 +21,7 @@ for i in range(100000000):
     lst.append('abcd')
 s = ''.join(lst)
 elapsed_time_join = timeit.default_timer() - start_time
-print('Time taken for string join:         ', elapsed_time_join)
+print('Time taken for string join:', elapsed_time_join)
 
 join_performance = (elapsed_time_concat - elapsed_time_join) / elapsed_time_concat
 print('Performance gained with join', join_performance)
